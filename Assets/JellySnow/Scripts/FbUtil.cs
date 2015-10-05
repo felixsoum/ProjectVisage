6605ab27dfe6835269941c67dead7c5aeda14b66
felixsoum <felix@felixsoum.com>
2015-10-05 00:06:29 -0400

    Remove type param for picture

--- Assets/JellySnow/Scripts/FbUtil.cs
+++ Assets/JellySnow/Scripts/FbUtil.cs
@@ -8,7 +8,7 @@
 	{
 		public static string GetPictureURL(string facebookID, int width, int height)
 		{
-			string url = string.Format("https://graph.facebook.com/{0}/picture?width={1}&height={2}", facebookID, width, height, type);
+			string url = string.Format("https://graph.facebook.com/{0}/picture?width={1}&height={2}", facebookID, width, height);
 			url += "&access_token=" + AccessToken.CurrentAccessToken.TokenString;
 			return url;
 		}
