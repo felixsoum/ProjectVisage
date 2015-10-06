a4617a7d688ae3a7337529cc9d10e73310bac8a9
blam <brianlamlook@gmail.com>
2015-10-05 23:13:50 -0400

    Add simple card game implementation.

--- Assets/JellySnow/Scripts/Util.cs
+++ Assets/JellySnow/Scripts/Util.cs
@@ -2,6 +2,7 @@
 {
 	using UnityEngine;
 	using System.Collections;
+	using System.Collections.Generic;
 
 	public static class Util
 	{
@@ -17,6 +18,23 @@
 		public static Sprite GetSprite(Texture2D tex)
 		{
 			return Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+		}
+		
+		public static void Shuffle<T>( this IList<T> list )
+		{
+			int n = list.Count;
+			while( n > 1 )
+			{
+				n--;
+				int k = n + 1;
+				while( k > n )
+				{
+					k = (int)(Random.value * (n + 1));
+				}
+				T value = list[k];
+				list[k] = list[n];
+				list[n] = value;
+			}
 		}
 	}
 }
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using JellySnow;
+
+public class Game : MonoBehaviour
+{
+	// Amount of cards in play.
+	public int numberOfCards = 54;
+
+	// Game objects to show the cards on
+	public GameObject cardTarget = null;
+	public GameObject cardGrid = null;
+
+	// Very simple implementation for testing
+	class CardData
+	{
+		public int Id { get; set; }
+	}
+	class CardDisplay
+	{
+		public GameObject GameObject { get; set; }
+	}
+	class CardInfo
+	{
+		public CardData Data { get; set; }
+		public CardDisplay Display { get; set; }
+	}
+
+	private List<CardData> cardsAll = new List<CardData>();
+	private List<CardData> cardsTarget = new List<CardData>();
+
+	private List<CardInfo> cardsInPlay = new List<CardInfo>();
+
+	void Awake()
+	{
+		// Put some random numbers to represent cards for now
+		for( int i = 0; i < 10000; ++i )
+		{
+			var card = new CardData();
+			card.Id = i;
+			cardsAll.Add( card );
+		}
+	}
+
+	void Start()
+	{
+		ShuffleCards();
+		DisplayCards();
+		ShowTargetCard();
+	}
+
+	void Update()
+	{
+	}
+
+	private void ShuffleCards()
+	{
+		foreach( var card in cardsInPlay )
+		{
+			Destroy( card.Display.GameObject );
+		}
+		cardsInPlay.Clear();
+		cardsTarget.Clear();
+		cardsAll.Shuffle();
+		for( int i = 0; i < numberOfCards; ++i )
+		{
+			cardsInPlay.Add( new CardInfo() { Data = cardsAll[i] } );
+			cardsTarget.Add( cardsAll[i] );
+		}
+		//cardsTarget.Shuffle();
+	}
+
+	private void DisplayCards()
+	{
+		foreach( var card in cardsInPlay )
+		{
+			card.Display = new CardDisplay() { GameObject = Instantiate( cardTarget ) };
+			card.Display.GameObject.transform.SetParent( cardGrid.transform, false );
+			card.Display.GameObject.GetComponent<Button>().interactable = true;
+			card.Display.GameObject.GetComponentInChildren<Text>().text = card.Data.Id.ToString();
+
+			var copy = card;
+			card.Display.GameObject.GetComponent<Button>().onClick.AddListener( () => OnClick_Card( copy ) );
+		}
+	}
+
+	private void ShowTargetCard()
+	{
+		cardTarget.GetComponentInChildren<Text>().text = cardsTarget[0].Id.ToString();
+		cardTarget.GetComponent<Animator>().SetTrigger( "Show" );
+	}
+
+	private void OnClick_Card( CardInfo card )
+	{
+		if( card.Data == cardsTarget[0] )
+		{
+			card.Display.GameObject.GetComponent<Image>().color = new Color( 0.0f, 0.0f, 0.0f, 0.0f );
+			card.Display.GameObject.GetComponent<Button>().interactable = false;
+			card.Display.GameObject.GetComponentInChildren<Text>().text = "";
+
+			cardsTarget.RemoveAt( 0 );
+			if( cardsTarget.Count == 0 )
+			{
+				ShuffleCards(); // Just restart for now
+				DisplayCards();
+			}
+			ShowTargetCard();
+		}
+		else
+		{
+			Debug.Log( "Wrong card pressed " + card.Data.Id );
+		}
+	}
+}
